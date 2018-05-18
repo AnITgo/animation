6c257fe5379fe943524960263a7d8986fcdf0c0e
yulong <844946414@qq.com>
2018-05-18 19:17:39 +0800

    0518

---
+++ animation/anmiation.js
@@ -0,0 +1,10 @@
+var container = $("#content");
+//页面可视区域
+var visualWidth = container.width();
+var visualHeight = container.height();
+
+var swipe = Swipe(container);
+//获取数据
+var getValue = function(className){
+  var $elem = $(""+ className +"");
+}
